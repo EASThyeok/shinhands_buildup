6f897b8785d4f2fbc7279c10dd5007dc16d9433a
EASThyeok <sia3246@gmail.com>
2022-09-23 16:56:40 +0900

    subnet2

--- subnet.tf
+++ subnet.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "main_subnet2" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.10.0/24"
+  cidr_block              = "10.0.20.0/24"
   availability_zone       = data.aws_availability_zones.available.names[2]
   map_public_ip_on_launch = true
   tags = {
